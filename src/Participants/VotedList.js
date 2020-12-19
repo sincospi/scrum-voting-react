e8b6a75a44ece51ed1793b943c00a61ab7f834cc
Dimitrios Spanos <dspanos@dialecticanet.com>
2020-12-19 23:09:18 +0200

    Refactors how voted users are sorted

--- src/Participants/VotedList.js
+++ src/Participants/VotedList.js
@@ -5,7 +5,22 @@ import User from "./UserVoted";
 export default function VotedList({ className, appState }) {
   const users = appState.connectedUsers
     .filter((u) => !!u.vote)
-    .sort((a, b) => (a.name || a.id).localeCompare(b.name || b.id))
+    .sort((a, b) => {
+      if (appState.revealVotes) {
+        if (isNaN(a.vote) && isNaN(b.vote)) {
+          return (a.name || a.id).localeCompare(b.name || b.id);
+        }
+        if (isNaN(a.vote)) {
+          return 1;
+        }
+        if (isNaN(b.vote)) {
+          return -1;
+        }
+        return parseInt(b.vote) - parseInt(a.vote);
+      } else {
+        return (a.name || a.id).localeCompare(b.name || b.id);
+      }
+    })
     .map((user) => (
       <User key={user.id} user={user} revealVote={appState.revealVotes} />
     ));
